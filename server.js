760c863c92f8bfedc079225ab0938a4721f6a13c
Predrag Milanovic <predrag.milanovic@apeiron-edu.eu>
2023-01-26 23:11:53 +0100

    add: express server

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-import './config.js';
+import config from './config.js';
+import apiRouter from './api/index.js';     //izbacuje mi gresku kada stavim samo './api'
+
+import express from 'express';
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send('Hello Express');
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+    console.info('Express listening on port ', config.port)
+});
