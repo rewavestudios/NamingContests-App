35eba9d5149bd0240a967d0b33e8b578340394ac
Predrag Milanovic <predrag.milanovic@apeiron-edu.eu>
2023-02-05 13:38:15 +0100

    fix: checksum problem

--- public/bundle.js
+++ public/bundle.js
@@ -12920,7 +12920,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 react_dom__WEBPACK_IMPORTED_MODULE_1__.render( /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement(_components_App__WEBPACK_IMPORTED_MODULE_2__["default"], {
-  initialContests: []
+  initialContests: window.initialData.contests
 }), document.getElementById('root'));
 })();
 
